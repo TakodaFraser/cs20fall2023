015267a599b432a19f0ba162031aadf8693f8325
27228021 <27228021@L815WD220009.edu.cbe.ab.ca>
2023-09-12 09:43:36 -0600

    9:43 am sept 11

---
+++ test 1/src/test2.java
@@ -0,0 +1,22 @@
+
+public class test2 {
+
+	public static void main(String[] args) {
+		
+		
+		//Declaration
+		int age;
+		double cost;
+		String name;
+		char Letter;
+		
+		//Initialization
+		age = 16;
+		cost = 45.50;
+		name = "name here";
+		Letter = 'f';
+		System.out.print(age+": "+(cost+name));
+		//System.out.print("type something else");
+	}
+
+}
